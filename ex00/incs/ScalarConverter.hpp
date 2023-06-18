4fdd47137c57f77552789aedf41f9cd941d14032
Mithras Kuipers <mithras.kuipers@neurolytics.ai>
2023-06-18 19:50:45 +0200

    updtae

---
+++ ex00/incs/ScalarConverter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScalarConverter.hpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/18 19:41:46 by mikuiper      #+#    #+#                 */
+/*   Updated: 2023/06/18 19:50:33 by mikuiper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER
+# define SCALARCONVERTER
+
+#include <iostream>
+
+class ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter const &other);
+		~ScalarConverter();
+
+	private:
+};
+	
+
+#endif
---
+++ ex00/srcs/ScalarConverter.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScalarConverter.cpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/18 19:42:44 by mikuiper      #+#    #+#                 */
+/*   Updated: 2023/06/18 19:50:19 by mikuiper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../incs/ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+	
+}
+
+ScalarConverter::ScalarConverter(ScalarConverter const &other)
+{
+	
+}
+
+ScalarConverter::~ScalarConverter()
+{
+	
+}

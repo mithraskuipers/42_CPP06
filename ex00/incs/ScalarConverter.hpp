cf508b6193fd51a640e27595e62b991009baf2f9
Mithras Kuipers <mithras.kuipers@neurolytics.ai>
2023-06-22 15:18:34 +0200

    old style, works fine, but overly complex. Will completely redo ex00

--- ex00/incs/ScalarConverter.hpp
+++ ex00/incs/ScalarConverter.hpp
@@ -1,12 +1,13 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-#include <iostream>
-#include <exception>
+#include <iostream> // input / output streams
+#include <exception> // handle exceptions
 #include <cstdlib> // For atoi and atof
-#include <string>
+#include <string> // string related
 #include <iomanip> // For std::fixed and std::setprecision
-
+#include <cctype> // character classification and conversion
+#include <sstream> // working with string streams
 
 class ScalarConverter
 {
@@ -15,7 +16,7 @@ public:
     ScalarConverter(const ScalarConverter& other);
     ~ScalarConverter();
     ScalarConverter& operator=(const ScalarConverter& other);
-    static void convert(const std::string& input);
+    static void convert(const std::string &input);
 
 private:
     static void convertChar(const std::string &input);
--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/18 19:42:44 by mikuiper      #+#    #+#                 */
-/*   Updated: 2023/06/21 23:42:34 by mikuiper      ########   odam.nl         */
+/*   Updated: 2023/06/22 15:13:44 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,141 +57,155 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
 
 void ScalarConverter::convert(const std::string &input)
 {
-    convertChar(input);
-    convertInt(input);
-    convertFloat(input);
-    convertDouble(input);
+	convertChar(input);
+	convertInt(input);
+	convertFloat(input);
+	convertDouble(input);
 }
 
-#include <cstdlib>
-#include <cctype>
-#include <iostream>
-#include <sstream>
-
-bool isInteger(const std::string& str)
+bool isInteger(const std::string &str)
 {
-    if (str.empty())
-        return false;
-
-    for (size_t i = 0; i < str.length(); ++i)
-    {
-        if (i == 0 && (str[i] == '-' || str[i] == '+'))
-            continue;
-
-        if (!std::isdigit(str[i]))
-            return false;
-    }
-
-    return true;
+	if (str.empty())
+	{
+		return false;
+	}
+	for (size_t i = 0; i < str.length(); ++i)
+	{
+		if (i == 0 && (str[i] == '-' || str[i] == '+'))
+		{
+			continue;
+		}
+		if (!std::isdigit(str[i]))
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 void ScalarConverter::convertChar(const std::string &input)
 {
-    std::cout << "char: ";
+	std::cout << "char: ";
 
-    // Check if the input is a valid integer
-    if (isInteger(input))
-    {
-        int value = std::atoi(input.c_str()); // Convert input to integer
-        char c = static_cast<char>(value);
-        if (isDisplayable(c))
-            std::cout << "'" << c << "'" << std::endl;
-        else
-            std::cout << "Non displayable" << std::endl;
-    }
-    else
-    {
-        std::istringstream iss(input);
-        float floatValue;
-        if (iss >> floatValue)
-        {
-            int intValue = static_cast<int>(floatValue);
-            char c = static_cast<char>(intValue);
-            if (isDisplayable(c))
-                std::cout << "'" << c << "'" << std::endl;
-            else
-                std::cout << "Non displayable" << std::endl;
-        }
-        else
-        {
-            std::cout << "impossible" << std::endl;
-        }
-    }
+	// Check if the input is a valid integer
+	if (isInteger(input))
+	{
+		int value = std::atoi(input.c_str()); // Convert input to integer
+		char c = static_cast<char>(value);
+		if (isDisplayable(c))
+			std::cout << "'" << c << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+	}
+	else
+	{
+		// Create an input string stream from the given input
+		std::istringstream iss(input);
+
+		// Declare a variable to store the floating-point value
+		float floatValue;
+
+		// Attempt to extract a float value from the input stream
+		if (iss >> floatValue)
+		{
+			// Convert the float value to an integer
+			int intValue = static_cast<int>(floatValue);
+
+			// Convert the integer value to a character
+			char c = static_cast<char>(intValue);
+
+			// Check if the character is displayable
+			if (isDisplayable(c))
+			{
+				// Display the character surrounded by single quotes
+				std::cout << "'" << c << "'" << std::endl;
+			}
+			else
+			{
+				// The character is not displayable
+				std::cout << "Non displayable" << std::endl;
+			}
+		}
+		else
+		{
+			// The input cannot be converted to a float value
+			std::cout << "impossible" << std::endl;
+		}
+	}
 }
 
 void ScalarConverter::convertInt(const std::string &input)
 {
-    std::cout << "int: ";
+	std::cout << "int: ";
 
-    // Check if the input is a valid integer
-    if (isInteger(input))
-    {
-        try
-        {
-            int i = std::atoi(input.c_str()); // Convert input to integer
-            std::cout << i << std::endl; // Print the integer value
-        }
-        catch(const std::exception& error)
-        {
-            std::cout << "impossible" << std::endl; // Conversion failed, print "impossible"
-        }
-    }
-    else
-    {
-        try
-        {
-            float f = std::atof(input.c_str()); // Convert input to float
-            int i = static_cast<int>(f); // Truncate float to integer
-            if (f == i)
-                std::cout << i << std::endl; // If float is an exact integer, print the integer value
-            else
-                std::cout << "impossible" << std::endl; // Float is not an exact integer, print "impossible"
-        }
-        catch(const std::exception& error)
-        {
-            std::cout << "impossible" << std::endl; // Conversion failed, print "impossible"
-        }
-    }
+	// Check if the input is a valid integer
+	if (isInteger(input))
+	{
+		try
+		{
+			int i = std::atoi(input.c_str()); // Convert input to integer
+			std::cout << i << std::endl; // Print the integer value
+		}
+		catch(const std::exception& error)
+		{
+			std::cout << "impossible" << std::endl; // Conversion failed, print "impossible"
+		}
+	}
+	else
+	{
+		try
+		{
+			float f = std::atof(input.c_str()); // Convert input to float
+			int i = static_cast<int>(f); // Truncate float to integer
+			if (f == i)
+				std::cout << i << std::endl; // If float is an exact integer, print the integer value
+			else
+				std::cout << "impossible" << std::endl; // Float is not an exact integer, print "impossible"
+		}
+		catch(const std::exception& error)
+		{
+			std::cout << "impossible" << std::endl; // Conversion failed, print "impossible"
+		}
+	}
 }
 
 void ScalarConverter::convertFloat(const std::string &input)
 {
-    std::cout << "float: ";
-    try
-    {
-        // Convert the input string to a float value
-        float f = std::atof(input.c_str());
+	std::cout << "float: ";
+	try
+	{
+		// Convert the input string to a float value
+		float f = std::atof(input.c_str());
 
-        // Set the precision to 1 digit after the decimal point
-		// With fixed you specify that floats are displayed in fixed-point notation and never in scientific notation.
+		// With fixed you specify that floats are displayed in fixed-point notation.
 		// With setprecision you specifiy that x digits are shown after the decimal point.
-        std::cout << std::fixed << std::setprecision(1); 
+		std::cout << std::fixed << std::setprecision(1); 
 
-        // Output the float value with the desired format
-        std::cout << f << "f" << std::endl;
-    }
-    catch (const std::exception& error)
-    {
-        std::cout << "nanf" << std::endl;
-    }
+		// Output the float value with the desired format
+		std::cout << f << "f" << std::endl;
+	}
+	catch (const std::exception& error)
+	{
+		std::cout << "nanf" << std::endl;
+	}
 }
 
-
+// convertDouble is practically identical to convertFloat
 void ScalarConverter::convertDouble(const std::string &input)
 {
-    std::cout << "double: ";
-    try
-    {
-        double d = std::atof(input.c_str());
-        std::cout << d << std::endl;
-    }
-    catch (const std::exception& error)
-    {
-        std::cout << "nan" << std::endl;
-    }
+	std::cout << "double: ";
+	try
+	{
+		double d = std::atof(input.c_str());
+		std::cout << d << std::endl;
+	}
+	catch (const std::exception& error)
+	{
+		std::cout << "nan" << std::endl;
+	}
 }
 
 bool ScalarConverter::isDisplayable(char c)
 {
-    return (c >= 32 && c <= 126);
+	return (c >= 32 && c <= 126);
 }
